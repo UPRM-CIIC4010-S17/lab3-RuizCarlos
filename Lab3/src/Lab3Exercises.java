d7ee8bdd06f316627f5205cee4899b655681ab91
CarlosDiego <CarlosDiego@192.168.2.8>
2017-02-08 17:12:41 -0400

    MyPanelClass Final

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -6,6 +6,8 @@ public class Lab3Exercises {
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(550, 300);
 		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 		//Same as the previous commit but with the push
 	}
